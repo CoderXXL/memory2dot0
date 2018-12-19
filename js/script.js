0629e960ee24dc6dfda3a84abdeb644bdd589d18
Dario Lang <dario.lang@flagbit.de>
2018-12-19 15:50:00 +0100

    [master] Add flip animation

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+  let card = document.getElementsByClassName('card');
+
+  for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener("click", function(event) {
+      card[i].classList.toggle('is-flipped');
+    })
+  };
+}
+
+
