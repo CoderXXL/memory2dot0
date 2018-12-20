454c02d0638754b62027d378f9365516130047d1
Dario Lang <dario.lang@flagbit.de>
2018-12-20 11:55:55 +0100

    [master] Render elements dynamic by image size, add background images, center background images on every size

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
 window.onload = () => {
+
+  this.renderCards(cardImages.length)
+
   this.setEventListener()
+
+}
+let card = document.getElementsByClassName('card');
+let cardBack = document.getElementsByClassName('card__face--back');
+let cardImages = ['Bild1', 'Bild2', 'Bild3', 'Bild4', 'Bild5', 'Bild6', 'Bild7', 'Bild8', 'Bild9'];
+
+function renderCards(size) {
+  for (let i = 0; i < size; i++) {
+    let newCard = document.createElement('div');
+    newCard.className = 'card back-side';
+    newCard.innerHTML = `
+    <div class="card__face card__face--front"></div>
+    <div class="card__face card__face--back"></div>
+    `;
+    field.appendChild(newCard);
+    this.setCardImage(i);
+  }
+};
+
+function setCardImage(i) {
+
+  return cardBack[i].style.backgroundImage = "url(../images/" + cardImages[i] + ".jpg)";
 }
 
 function setEventListener() {
-  let card = document.getElementsByClassName('card');
-
   for (let i = 0; i < card.length; i++) {
-    card[i].addEventListener("click", function(event) {
-      card[i].classList.toggle('is-flipped');
+    card[i].addEventListener("click", function() {
+      card[i].classList.toggle('flipped');
     })
   };
 }
+
