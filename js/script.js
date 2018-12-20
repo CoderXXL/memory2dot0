07c56b0efe1dd489e5c1ee13f293460c814c95e9
Dario Lang <dario.lang@flagbit.de>
2018-12-20 13:29:55 +0100

    [master] Add math.random and dumblicate array

--- js/script.js
+++ js/script.js
@@ -5,11 +5,14 @@ window.onload = () => {
   this.setEventListener()
 
 }
+
 let card = document.getElementsByClassName('card');
 let cardBack = document.getElementsByClassName('card__face--back');
-let cardImages = ['Bild1', 'Bild2', 'Bild3', 'Bild4', 'Bild5', 'Bild6', 'Bild7', 'Bild8', 'Bild9'];
+let cardImages = shuffle(['Bild1', 'Bild2', 'Bild3', 'Bild4', 'Bild5', 'Bild6', 'Bild7', 'Bild8',
+                          'Bild1', 'Bild2', 'Bild3', 'Bild4', 'Bild5', 'Bild6', 'Bild7', 'Bild8']);
 
 function renderCards(size) {
+  console.log(cardImages)
   for (let i = 0; i < size; i++) {
     let newCard = document.createElement('div');
     newCard.className = 'card back-side';
@@ -24,7 +27,8 @@ function renderCards(size) {
 
 function setCardImage(i) {
 
-  return cardBack[i].style.backgroundImage = "url(../images/" + cardImages[i] + ".jpg)";
+  //return cardBack[i].style.backgroundImage = "url(../images/" + cardImages[i] + ".jpg)";
+  return cardBack[i].innerHTML = cardImages[i];
 }
 
 function setEventListener() {
@@ -35,3 +39,15 @@ function setEventListener() {
   };
 }
 
+function shuffle(array) {
+  let i = array.length, a = 0, temp1;
+
+  while (i--) {
+      a = Math.floor(Math.random() * (i+1));
+      temp1 = array[i];
+      array[i] = array[a];
+      array[a] = temp1;
+  }
+
+  return array;
+}
